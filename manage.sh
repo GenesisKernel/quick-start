0965ac360f4ad9bf0182157369c3ac6f162624bf
blitzstern5 <blitzstern5@gmail.com>
2018-07-12 02:59:27 +0300

    Change scripts branch to master

--- manage.sh
+++ manage.sh
@@ -19,7 +19,7 @@ else
 fi
 FRONTEND_BRANCH="master"
 SCRIPTS_REPO_URL="https://github.com/blitzstern5/genesis-scripts"
-SCRIPTS_BRANCH="update-sign"
+SCRIPTS_BRANCH="master"
 DEMO_APPS_URL="https://raw.githubusercontent.com/GenesisKernel/apps/master/quick-start/quick-start.json"
 
 DB_NAME_PREFIX="genesis"
