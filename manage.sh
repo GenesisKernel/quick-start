b752ba124672da7f88ee69f8a2c9f6b591aa4d15
blitzstern5 <blitzstern5@gmail.com>
2019-04-03 06:37:14 +0300

    Update fast install data

--- manage.sh
+++ manage.sh
@@ -29,8 +29,8 @@ GAP_BETWEEN_BLOCKS=8
 MAX_NUM_OF_NODES=5
 
 if [ "$USE_PRODUCT" = "apla" ]; then
-    FAST_INSTALL_DATA_URL="https://github.com/blitzstern5/quick-start-data/raw/master/apla/0.9.3/apla-qs-0.9.3-fast-install-data-20190402133432-1-nodes.tar.gz"
-    FAST_INSTALL_DATA_BASENAME="apla-qs-0.9.3-fast-install-data-20190402133432-1-nodes.tar.gz"
+    FAST_INSTALL_DATA_URL="https://github.com/blitzstern5/quick-start-data/raw/master/apla/0.9.4/apla-qs-0.9.4-fast-install-data-20190403060559-1-nodes.tar.gz"
+    FAST_INSTALL_DATA_BASENAME="apla-qs-0.9.4-fast-install-data-20190403060559-1-nodes.tar.gz"
 else
     FAST_INSTALL_DATA_URL="https://github.com/blitzstern5/quick-start-data/raw/master/genesis/0.9.4/genesis/0.9.4/genesis-qs-0.9.4-fast-install-data-20190403052958-1-nodes.tar.gz"
     FAST_INSTALL_DATA_BASENAME="genesis/0.9.4/genesis-qs-0.9.4-fast-install-data-20190403052958-1-nodes.tar.gz"
