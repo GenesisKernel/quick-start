40f7dfe457d552d0dc782dfb5e49f4cce5b37f3d
blitzstern5 <blitzstern5@gmail.com>
2019-04-03 06:46:40 +0300

    Fix genesis and apla fast install basename

--- manage.sh
+++ manage.sh
@@ -32,7 +32,7 @@ if [ "$USE_PRODUCT" = "apla" ]; then
     FAST_INSTALL_DATA_URL="https://github.com/blitzstern5/quick-start-data/raw/master/apla/0.9.4/apla-qs-0.9.4-fast-install-data-20190403060559-1-nodes.tar.gz"
     FAST_INSTALL_DATA_BASENAME="apla-qs-0.9.4-fast-install-data-20190403060559-1-nodes.tar.gz"
 else
-    FAST_INSTALL_DATA_URL="https://github.com/blitzstern5/quick-start-data/raw/master/genesis/0.9.4/genesis/0.9.4/genesis-qs-0.9.4-fast-install-data-20190403052958-1-nodes.tar.gz"
+    FAST_INSTALL_DATA_URL="https://github.com/blitzstern5/quick-start-data/raw/master/genesis/0.9.4/genesis-qs-0.9.4-fast-install-data-20190403052958-1-nodes.tar.gz"
     FAST_INSTALL_DATA_BASENAME="genesis-qs-0.9.4-fast-install-data-20190403052958-1-nodes.tar.gz"
 fi    
 
